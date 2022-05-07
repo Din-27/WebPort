60f95e77808f77d7d3f2e3c4d44d153375ff741f
Herdiyana firmansyah <Herdiyana-gif>
2022-05-07 17:14:48 +0700

    update

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import a from '../../assets/birth.jpeg'
+import b from '../../assets/WhatsApp Image 2022-01-22 at 19.42.36.jpeg'
+import c from '../../assets/WhatsApp Image 2022-01-22 at 19.42.37 (2).jpeg'
+import d from '../../assets/WhatsApp Image 2022-01-22 at 19.42.37.jpeg'
+import e from '../../assets/WhatsApp Image 2022-01-22 at 19.42.38 (1).jpeg'
 
 function Carousel() {
   return (
     <div class="carousel rounded-box w-96">
         <div class="carousel-item w-1/2">
+            <img src={a} class="w-full" />
+        </div> 
+        <div class="carousel-item w-1/2">
+            <img src={b} class="w-full" />
+        </div> 
+        <div class="carousel-item w-1/2">
+            <img src={c} class="w-full" />
+        </div> 
+        <div class="carousel-item w-1/2">
+            <img src={d} class="w-full" />
+        </div> 
+        <div class="carousel-item w-1/2">
+            <img src={e} class="w-full" />
+        </div> 
+        {/* <div class="carousel-item w-1/2">
             <img src="https://api.lorem.space/image/game?w=256&h=400&hash=8B7BCDC2" class="w-full" />
         </div> 
         <div class="carousel-item w-1/2">
@@ -23,7 +43,7 @@ function Carousel() {
         </div> 
         <div class="carousel-item w-1/2">
             <img src="https://api.lorem.space/image/game?w=256&h=400&hash=7F5AE56A" class="w-full" />
-        </div>
+        </div> */}
     </div>
   )
 }
